cb64e80b6407c31cea84f62273cb652377877355
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-15 11:58:10 -0500

    Upadted DeviceOSIcon object to load dictionaries with option when the object is created, next: to select individual items passed on input params

--- Models/DeviceOSIcon.cs
+++ Models/DeviceOSIcon.cs
@@ -5,16 +5,26 @@ namespace back_end.Models
 {
     public class DeviceOSIcon
     {
-
+       
         public DeviceOSIcon()
         {
 
         }
 
-        public DeviceOSIcon(string deviceOs, string deviceType)
-        {
-            var device = deviceIconList.Where(x => x.Value == deviceType.AsEnumerable()).Select(x => x.Key).FirstOrDefault();
-            var os =  deviceOSIconList.Where(x => x.Value == deviceOs.AsEnumerable()).Select(x => x.Key).FirstOrDefault();
+        public DeviceOSIcon(string deviceOS, string deviceType)
+        {   
+            //create two dictionaries to hold Device and OS Names and Icons
+            Dictionary<string, List<string>> newDeviceNameAndIconDictionary = new Dictionary<string, List<string>>();
+
+            Dictionary<string, List<string>> newOSNameAndIconDictionary = new Dictionary<string, List<string>>();
+
+            //populated the Device Name and Icon with our Device Name and Icon lists
+            newDeviceNameAndIconDictionary = NewDictionaryList(DeviceNameList, DeviceIconList);
+
+            //populated the Device Name and Icon with our Device Name and Icon lists
+            newOSNameAndIconDictionary = NewDictionaryList(DeviceOSNameList, DeviceOSIconList);
+
+
         }
 
         //Icons and names for Device
@@ -22,14 +32,20 @@ namespace back_end.Models
         //These dictionary collections will be used to display information on the device-list bootstrap rows and creation and display of a since device, they will be tied into the device as soon as it's created, so the device will always carry these names and icon items with it
         //List<string> types to combine into a device name and icon dictionary collection
 
+        //these two lists for Device Name and Icon dictionary
         public static List<string> DeviceNameList = new List<string> { "Desktop", "Laptop", "Phone", "Tablet" };
         public static List<string> DeviceIconList = new List<string> { "fas fa - desktop", "fas fa - laptop - code", "fas fa - mobile - alt", "fas fa - tablet - alt" };
-
-        public static Dictionary<string, List<string>> deviceIconList = DeviceNameList.Select(x => new { key = x, value = DeviceIconList }).ToDictionary(e => e.key, e => e.value);
-        //List<string> types to combine into a device name and icon dictionary collection
+        //these two lists for OS Name and Icon dictionary    
         public static List<string> DeviceOSNameList = new List<string> { "Windows", "MacOS", "Apple", "Android" };
         public static List<string> DeviceOSIconList = new List<string> { "fa fa-windows", "fas fa -laptop", "fab fa-apple", "fab fa-android" };
 
-        public static Dictionary<string, List<string>> deviceOSIconList = DeviceOSNameList.Select(x => new { key = x, value = DeviceOSIconList }).ToDictionary(e => e.key, e => e.value);
+        Dictionary<string, List<string>> NewDictionaryList(List<string> keyList, List<string> valueList)
+        {
+            Dictionary<string, List<string>> deviceOSIconList = keyList.Select(x => new { key = x, value = valueList }).ToDictionary(e => e.key, e => e.value);
+            return deviceOSIconList;
+
+        }
     }
 }
+
+
