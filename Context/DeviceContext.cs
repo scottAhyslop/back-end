c3ea0b86ffb9d1f6cd799aa3e461e36326d4aa9c
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-19 22:32:30 -0500

    EF Migration failed, commiting revert to previous commit

---
+++ Context/DeviceContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using back_end.Models;
+
+#nullable disable
+
+namespace back-end
+{
+    public partial class DevicveContext : DbContext
+    {
+        public DevicveContext()
+        {
+        }
+
+        public DevicveContext(DbContextOptions<DevicveContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Device> Devices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(e => e.DeviceIconPath)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DeviceName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DeviceOs)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("DeviceOS");
+
+                entity.Property(e => e.DeviceOsiconPath)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("DeviceOSIconPath");
+
+                entity.Property(e => e.DeviceStatus)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DeviceType)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Temperature)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.TimeInUse)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- Models/Device.cs
+++ Models/Device.cs
@@ -1,82 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
+#nullable disable
 
 namespace back_end.Models
 {
-    public class Device
+    public partial class Device
     {
-        private int _deviceId;
-        //DeviceId incrementer holder value
-        private static int deviceIdIncrementer = 0;
-       //DeviceID auto generated upon creation
-        public int DeviceId 
-        { get {return _deviceId;}
-          set { if (value!=0)
-                {
-                    _deviceId=value;
-                } else if(value==0)
-                {
-                     throw new NullReferenceException();
-                }
-          }
-         }
-        
-
-        //blank constructor
-        public Device()
-        {
-        }
-        //these values will be pulled from data to populate the device, all values can be null, except for DeviceId which is created upon instantiation, and temp which will be wired to the device
-        public Device(string deviceName, float temp, List<string> deviceIconPath, List<string> deviceOSIconPath, string deviceOS, string deviceType, string deviceStatus, TimeSpan timeInUse)
-        {
-            //create a new DeviceId using system clock to give each instance a unique id    
-            this.DeviceId = System.Threading.Interlocked.Increment(ref deviceIdIncrementer);
-
-            //Properties that may or may not be null depending on what data is passed in
-            DeviceName = deviceName;
-            if (DeviceName == null)
-            {
-                DeviceName="New Device";
-            }
-            if (temp == 0)
-            {
-                //assuming the temp of the Device is being tracked and updated by the tick...
-                Temperature = this.Temperature;
-            }
-            //These fields will be chosen by the user in AddDevice
-            //Drop-down menus in forms that will show icons co-relating to the OSs and Devices chosen
-            //The icons, devices, and OSs currently can be populated into dictionaries by methods found DeviceOSIcon
-            /* DeviceIconPath = deviceIconPath;
-            DeviceOSIconPath = deviceOSIconPath; */
-            DeviceOS = deviceOS;
-            DeviceType = deviceType;
-            DeviceOSIcon newDeviceOSIcon = new DeviceOSIcon(deviceIconPath.ToString(), deviceOSIconPath.ToString());
-            
-            //a general health of the machine check, assuming that there is an active routine montioring 'health' params and that this app would have access to that, in the meantime, it's a string
-            DeviceStatus = deviceStatus;
-            if (DeviceStatus==null)
-            {
-                DeviceStatus="Unknown";
-            }
-        }
-
-       
+        public int DeviceId { get; set; }
         public string DeviceName { get; set; }
-        public float? Temperature { get; set; }
+        public string Temperature { get; set; }
+        public string DeviceIconPath { get; set; }
+        public string DeviceOsiconPath { get; set; }
         public string DeviceType { get; set; }
-        public string DeviceOS { get; set; }
-        
+        public string DeviceOs { get; set; }
         public string DeviceStatus { get; set; }
-        public string AdminEyesOnly { get; set; } 
-        public TimeSpan? TimeInUse { get; set; }
-        public Dictionary<string, List<string>> DeviceIconPath = new Dictionary<string, List<string>>();
-        public Dictionary<string, List<string>> DeviceOSIconPath = new Dictionary<string, List<string>>();
-
-        
-        
-
+        public string TimeInUse { get; set; }
     }
-        
-
 }
