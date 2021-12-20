153fa5af9fe2de8bb1099a9b45922be31a71ad84
Scott A. Hyslop <sahyslop@gmail.com>
2021-12-19 23:50:32 -0500

    Db connection established, changes to BackEndContext, Startup, and DevicesController

---
+++ Models/BackEndContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace back_end.Models
+{
+    public class BackEndContext: DbContext
+    {
+        public BackEndContext(DbContextOptions<BackEndContext> options): base(options)
+        {}
+        public DbSet<Device> Devices { get; set; }
+
+    }
+}
